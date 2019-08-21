dc6bc8ba15b48f36c85bea65f50b99ba35496e25
msahu2595 <msahu2595@gmail.com>
2019-08-21 13:58:23 +0530

    commit

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -8,7 +8,7 @@ import thunk from "redux-thunk";
 
 const middlewares = [thunk];
 
-if (process.env.NODE_ENV === `production`) {
+if (process.env.NODE_ENV === `development`) {
   const { logger } = require(`redux-logger`);
 
   middlewares.push(logger);
