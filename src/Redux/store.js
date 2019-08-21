381f42410b6d13038b3e6d620a4831f0e44d2aa3
msahu2595 <msahu2595@gmail.com>
2019-08-21 13:56:28 +0530

    normal.

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,15 +3,27 @@ import rootReducer from "./reducers/rootReducer";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
+
+// import logger from "redux-logger";
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === `production`) {
+  const { logger } = require(`redux-logger`);
+
+  middlewares.push(logger);
+}
 
 const persistConfig = {
   key: "root",
   storage
 };
 
-export let store = createStore(
-  persistReducer(persistConfig, rootReducer),
-  applyMiddleware(thunk, logger)
-);
-export let persistor = persistStore(store);
+export default () => {
+  let store = createStore(
+    persistReducer(persistConfig, rootReducer),
+    applyMiddleware(...middlewares)
+  );
+  let persistor = persistStore(store, {}, () => {});
+  return { store, persistor };
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import { Provider } from "react-redux";
-import { store, persistor } from "./Redux/store";
-
 import { PersistGate } from "redux-persist/integration/react";
 
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
+import storeFunc from "./Redux/store";
 import * as serviceWorker from "./serviceWorker";
+const { store, persistor } = storeFunc();
 
 ReactDOM.render(
   <Provider store={store}>
